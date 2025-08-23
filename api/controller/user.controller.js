a08e1901d67de2656a0b2b1198643b4f62f2001c
Alokdev07 <alokbhuyan162@gmail.com>
2025-08-23 12:30:01 +0530

    cors added

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,5 +1,3 @@
-import { User } from '../models/user.model.js'
-import { ApiError } from '../utils/ApiError.js'
 
 
 import { User } from "../models/user.model.js";
--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -10,6 +11,10 @@ app.use(express.json({limit : "20kb"}))
 app.use(express.urlencoded({extended : true}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use(cors({
+    origin : 'http://localhost:5173',
+    credentials : true
+},))
 
 app.get('/',(req,res) => {
     res.json("hello world")
